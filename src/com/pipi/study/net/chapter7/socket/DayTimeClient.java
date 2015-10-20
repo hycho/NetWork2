52fcf44f109f1e9c49e65297ef5e8ae7ef77b7ff
Administrator <Administrator@MSDN-SPECIAL>
2015-10-20 23:33:31 +0900

    socket 예제 추가

---
+++ src/com/pipi/study/net/chapter7/socket/DayTimeClient.java
@@ -0,0 +1,34 @@
+package com.pipi.study.net.chapter7.socket;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class DayTimeClient {
+
+	public static void main(String[] args) {
+		String hostname = "time.nist.gov";
+		Socket socket = null;
+		
+		try {
+			socket = new Socket(hostname, 13);
+			socket.setSoTimeout(15000); //만약 15초동안 아무런 연결을 하지 못할 경우 SocketTimeoutException 예외를 발생시킨다.
+			InputStream in = socket.getInputStream();
+			StringBuilder time = new StringBuilder();
+			InputStreamReader reader = new InputStreamReader(in);
+			for(int c = reader.read(); c!=-1; c=reader.read()) {
+				time.append((char) c);
+			}
+			System.out.println(time);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
